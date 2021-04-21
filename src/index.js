32b0520b9ae8a423c7918af812ac6bdfe1d578a8
Sergio99778 <sergio4brandon@gmai.com>
2021-04-21 13:57:59 -0500

    index.js added.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,46 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+ const url = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+ fetch(url)
+ .then(respuesta => respuesta.json())
+ .then((responseJson) => {
+     const allItems = [];
+     responseJson.data.forEach(item => {
+         const imagen = document.createElement("img");
+         const title = document.createElement("h2");
+         const price = document.createElement("div");
+         const container = document.createElement("div");
+         container.append(imagen, title, price);
+         allItems.push(container)
+     });
+     document.body.appendChild(...allItems)
+ });
+
+ 
+
+ /* //ASYNC AWAIT
+ const url = "https://platzi-avo.vercel.app/api/avo";
+
+ async function fetchData() {
+     const response = await fetch(url),
+     data = await response.json(),
+     allItems = [];
+
+     data.data.forEach((item) => {
+        const image = document.createElement("img");
+        // create title
+        const title = document.createElement("h2");
+        // create price
+        const price = document.createElement("div");
+    
+        const container = document.createElement("div");
+        container.append(image, title, price);
+    
+        allItems.push(container);
+     });
+     document.body.append(...allItems);
+ }
+
+ fetchData(): */
