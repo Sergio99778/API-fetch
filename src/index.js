d491466125f1dbeae52936aea56bef03cd17e6bc
Sergio99778 <sergio4brandon@gmai.com>
2021-04-27 12:45:56 -0500

    Enriqueciendo aplicacion.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,26 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
- const url = "https://platzi-avo.vercel.app/api/avo";
+ const baseUrl = "https://platzi-avo.vercel.app";
 
- fetch(url)
+const appNode = document.querySelector('#app');
+
+ fetch(`${baseUrl}/api/avo`)
  .then(respuesta => respuesta.json())
  .then((responseJson) => {
      const allItems = [];
      responseJson.data.forEach(item => {
          const imagen = document.createElement("img");
+         imagen.src = `${baseUrl}${item.image}`
          const title = document.createElement("h2");
+         title.textContent = item.name;
          const price = document.createElement("div");
+         price.textContent = item.price;
          const container = document.createElement("div");
          container.append(imagen, title, price);
          allItems.push(container)
      });
-     document.body.appendChild(...allItems)
+     appNode.append(...allItems)
  });
 
  
